495c922e802a484ba53828e9a3397d16adda4042
emmanuel.eban <82966135+eebbann@users.noreply.github.com>
2023-07-11 13:32:35 -0500

    TodoItem refactored to function

--- src/components/functionComp/TodoItem.jsx
+++ src/components/functionComp/TodoItem.jsx
@@ -1,28 +1,51 @@
-import React from 'react'
+import React from 'react';
 
-export default function TodoItem(prop) {
-	const{id, title, complete, ocComplete} = props;
+function TodoItem(prop) {
+	const { id, title, complete, ocComplete } = props;
 
 	//oncompleteCheck function
-	const _onCompleteCheck = (e)=>{
+	const _onCompleteCheck = (e) => {
 		_onCompleteCheck(id);
-	}
-	
-	const _renderCheckbox =()=>{
-		const attrs ={};
+	};
 
-		if(complete){
+	const _renderCheckbox = () => {
+		const attrs = {};
+
+		if (complete) {
 			attrs.checked = "checked";
 		}
-	}
-	return (
-		<div className="col-2 todo-item__checkbox">
-			<input
-			type='checkbox'
-			className='form-control'
-      onChange={_onCompleteCheck}
-			{...attrs}/>
 
-		</div>
-	)
-}
+		return (
+			<div className="col-2 todo-item__checkbox">
+				<input
+					type='checkbox'
+					className='form-control'
+					onChange={_onCompleteCheck}
+					{...attrs} />
+
+			</div>
+
+
+		);
+	};
+
+	const _renderTitle = () => {
+		return (
+			<div className="col-10 todo-item__title">
+				<h3>{title}</h3>
+			</div>
+		);
+	};
+
+	return (
+		<li className="list-group-item todo-item">
+			<div className="row">
+				{_renderCheckbox()}
+				{_renderTitle()}
+			</div>
+		</li>
+	);
+};
+
+export default TodoItem;
+
